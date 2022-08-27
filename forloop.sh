8e6759fca558d48bb1e6d7c89a86e3f2a0e2c7c3
Vicky Kumar <111136233+vicky0004@users.noreply.github.com>
2022-08-27 09:36:25 +0530

    Add files via upload

---
+++ forloop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+#for i in 1 2 3 4 5
+#do
+#	echo "by example 1- $i"
+#done
+
+#for i in {1..5}
+#do
+#	echo "BY example 2- $i"
+#done
+
+for (( i=1; i<=5; i++))
+do
+	echo "by example 3- $i"
+done
